eeaa2e64ca1b73eb92f97f65bc92394b0a89c881
stozuka <stozuka@gmail.com>
2017-11-25 15:21:48 +0800

    updates

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :discuss, Discuss.Endpoint,
   url: [scheme: "https", host: "phoenixdiscuss.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info

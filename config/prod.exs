6172efc1bb36b4360576dccfc79416ee572b4c83
stozuka <stozuka@gmail.com>
2017-11-25 14:43:31 +0800

    updates

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :discuss, Discuss.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "phoenixdiscuss.herokuapp.com", port: 443],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production

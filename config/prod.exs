620c979abde6dbcebde0bd5d8c236e8b9df7e432
stozuka <stozuka@gmail.com>
2017-11-25 15:20:15 +0800

    updates

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :discuss, Discuss.Endpoint,
   http: [port: {:system, "PORT"}],
   # url: [scheme: "https", host: "phoenixdiscuss.herokuapp.com", port: 443],
-  url: [scheme: "https", host: "localhost", port: 443],
+  url: [scheme: "https", host: "127.0.0.1", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),

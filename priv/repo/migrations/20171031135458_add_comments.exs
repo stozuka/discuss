d9a35dd9fb5aca0c3d2909d7e9470b8fc28f7cc4
stozuka <stozuka@gmail.com>
2017-11-25 18:31:58 +0800

    updates

--- priv/repo/migrations/20171031135458_add_comments.exs
+++ priv/repo/migrations/20171031135458_add_comments.exs
@@ -4,8 +4,8 @@ defmodule Discuss.Repo.Migrations.AddComments do
   def change do
     create table(:comments) do
       add :content, :string
-      add :user_id, references(:users)
-      add :topic_id, references(:topics)
+      add :user_id, references(:users, on_delete: :delete_all)
+      add :topic_id, references(:topics, on_delete: :delete_all)
 
       timestamps()
     end
